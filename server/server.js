34142ef175aae6bbfaf4956ba4104550b04a4ac2
thechutrain <chu.chuyalan@gmail.com>
2017-07-29 12:57:18 -0400

    something to help me debug I hope

--- server/server.js
+++ server/server.js
@@ -31,17 +31,31 @@ app.use(
 	})
 )
 // ===== testing middleware =====
-app.use(function(req, res, next) {
-	console.log('===== passport user =======')
-	console.log(req.session)
-	console.log(req.user)
-	console.log('===== END =======')
-	next()
-})
+// app.use(function(req, res, next) {
+// 	console.log('===== passport user =======')
+// 	console.log(req.session)
+// 	console.log(req.user)
+// 	console.log('===== END =======')
+// 	next()
+// })
 
 // ===== Passport ====
 app.use(passport.initialize())
 app.use(passport.session())
+
+// testing
+app.get(
+	'/auth/google/callback',
+	(req, res, next) => {
+		console.log(`req.user: ${req.user}`)
+		console.log('======= /auth/google/callback was called! =====')
+		next()
+	},
+	passport.authenticate('google', { failureRedirect: '/login' }),
+	(req, res) => {
+		res.redirect('/')
+	}
+)
 
 // ==== if its production environment!
 if (process.env.NODE_ENV === 'production') {
