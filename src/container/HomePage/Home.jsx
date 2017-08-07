87b20170e42e2f3355f0c57bf812853d3f68d724
thechutrain <chu.chuyalan@gmail.com>
2017-08-07 19:26:52 -0400

    Home is not a stateful component that is wrapped around the isLoading HOC to display the spinner while waiting to get user information

--- src/container/HomePage/Home.jsx
+++ src/container/HomePage/Home.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
+import withLoading from '../../components/isLoadingHOC'
 import SecretData from './SecretData'
 // TODO - add proptypes
 
-const Home = props => {
-	if (props.user) {
-		return (
-			<div className="Home">
-				<p>Current User:</p>
-				<code>
-					{JSON.stringify(props)}
-				</code>
-				<SecretData />
-			</div>
-		)
-	} else {
-		return (
-			<div className="Home">
-				<h2>This is the home page</h2>
-				<p>Current User:</p>
-				<code>
-					{JSON.stringify(props)}
-				</code>
-				<SecretData />
-			</div>
-		)
+class Home extends Component {
+	constructor() {
+		super()
+	}
+	render() {
+		if (this.props.user) {
+			return (
+				<div className="Home">
+					<p>Current User:</p>
+					<code>
+						{JSON.stringify(this.props)}
+					</code>
+					<SecretData />
+				</div>
+			)
+		} else {
+			return (
+				<div className="Home">
+					<h2>This is the home page</h2>
+					<p>Current User:</p>
+					<code>
+						{JSON.stringify(this.props)}
+					</code>
+					<SecretData />
+				</div>
+			)
+		}
 	}
 }
 
-export default Home
+export default withLoading(Home, true)
