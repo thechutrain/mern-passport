ddc2b254df719eab8e02a4e103d55288ce64a043
thechutrain <chu.chuyalan@gmail.com>
2017-08-07 19:25:56 -0400

    took out the display links into a separate file and am keeping track of isLoading now

--- src/App.jsx
+++ src/App.jsx
@@ -2,80 +2,45 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Route, Link } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import LoginForm from './container/LoginPage/Login/LoginForm'
 import SignupForm from './container/LoginPage/SignupForm'
 import Home from './container/HomePage/Home'
-
-const DisplayLinks = props => {
-	if (props.loggedIn) {
-		return (
-			<nav className="navbar">
-				<ul className="nav">
-					<li className="nav-item">
-						<Link to="/" className="nav-link">
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link to="/" className="nav-link" onClick={props._logout}>
-							Logout
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		)
-	} else {
-		return (
-			<nav className="navbar">
-				<ul className="nav">
-					<li className="nav-item">
-						<Link to="/" className="nav-link">
-							Home
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link to="/login" className="nav-link">
-							login
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link to="/signup" className="nav-link">
-							sign up
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		)
-	}
-}
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			loggedIn: false,
-			user: null
+			loggedIn: null,
+			user: null,
+			isLoading: true
 		}
 		this._logout = this._logout.bind(this)
 		this._login = this._login.bind(this)
 	}
 	componentDidMount() {
-		axios.get('/auth/user').then(response => {
-			console.log(response.data)
-			if (!!response.data.user) {
-				console.log('THERE IS A USER')
-				this.setState({
-					loggedIn: true,
-					user: response.data.user
+		// ====== for testing the loading purposes =======
+		// TODO - remove the setTimeout later ...
+		setTimeout(
+			function() {
+				axios.get('/auth/user').then(response => {
+					if (!!response.data.user) {
+						this.setState({
+							loggedIn: true,
+							user: response.data.user,
+							isLoading: false
+						})
+					} else {
+						this.setState({
+							loggedIn: false,
+							user: {},
+							isLoading: false
+						})
+					}
 				})
-			} else {
-				this.setState({
-					loggedIn: false,
-					user: null
-				})
-			}
-		})
+			}.bind(this),
+			2000
+		)
 	}
 
 	_logout(event) {
@@ -86,7 +51,7 @@ class App extends Component {
 			if (response.status === 200) {
 				this.setState({
 					loggedIn: false,
-					user: null
+					user: {}
 				})
 			}
 		})
@@ -113,11 +78,23 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				{/* -- HEADER COMPONENT --
+					* contains Navigation bar with LINKS
+					* Message to user etc.
+					*/}
 				{/* <Header user={this.state.user} /> */}
 				{/* LINKS to our different 'pages' */}
-				<DisplayLinks _logout={this._logout} loggedIn={this.state.loggedIn} />
+				{/* <DisplayLinks _logout={this._logout} loggedIn={this.state.loggedIn} /> */}
+				<Header
+					_logout={this._logout}
+					{...this.state}
+					// loggedIn={this.state.loggedIn}
+					// user={this.state.user}
+					// isLoading={this.state.isLoading}
+				/>
 				{/*  ROUTES */}
-				<Route exact path="/" render={() => <Home user={this.state.user} />} />
+				<Route exact path="/" render={() => <Home {...this.state} />} />
+
 				<Route
 					exact
 					path="/login"
