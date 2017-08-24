0203b5939c9f04f5b23b6634009c8c05c22a1e61
thechutrain <chu.chuyalan@gmail.com>
2017-08-24 10:05:41 -0400

    added provider and store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import App from './React/App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+// Redux dependencies
+import { Provider } from 'react-redux'
+import { store } from './Redux/store'
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider store={store} >
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 )
