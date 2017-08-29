14a9b2885a9b9e452ffde6fba1aef3b8ec3f74b2
thechutrain <chu.chuyalan@gmail.com>
2017-08-29 00:37:25 -0400

    no more flashMsg in authentication, logic is in a separate reducer

--- src/Redux/authentication.js
+++ src/Redux/authentication.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import { updateMsg } from './alerts'
 /* ======= Action Types =========
 *
 */
 export const LOCAL_SIGN_IN = 'LOCAL_SIGN_IN'
 export const LOCAL_SIGN_UP = 'LOCAL_SIGN_UP'
-export const FLASH_MSG = 'FLASH_MSG'
+// export const FLASH_MSG = 'FLASH_MSG'
 // export const FLASH_MSG_ERROR = 'FLASH_MSG_ERROR'
 // export const FLASH_MSG_SUCCESS = 'FLASH_MSG_SUCCESS'
 export const SIGN_OUT = 'SIGN_OUT'
@@ -17,11 +18,11 @@ const defaultState = {
   user: {
   },
   // for updating to the user that they've signed in or not
-  flashMsg: {
-    error: false,
-    displayMsg: false,
-    msg: '',
-  }
+  // flashMsg: {
+  //   error: false,
+  //   displayMsg: false,
+  //   msg: '',
+  // }
 }
 
 export default function reducer(state = defaultState, action) {
@@ -33,8 +34,8 @@ export default function reducer(state = defaultState, action) {
       return { ...state, user: action.payload.user, loggedIn: true }
     // case LOCAL_REGISTER:
     //   return 
-    case FLASH_MSG:
-      return { ...state, flashMsg: { ...state.flashMsg, ...action.payload.flashMsg } }
+    // case FLASH_MSG:
+    //   return { ...state, flashMsg: { ...state.flashMsg, ...action.payload.flashMsg } }
     default:
       return state
   }
@@ -47,28 +48,34 @@ export default function reducer(state = defaultState, action) {
 export const signOut = () => (dispatch, getState) => {
   // making the axios post to route clearsCookie storing session id
   return axios.post('/auth/logout').then(()=> {
+    // throw new Error('yep')
     dispatch({ type: SIGN_OUT })
+    dispatch(updateMsg('Successful signin', {success:true}, 5))
   }).catch((error) => {
-    dispatch({ type: FLASH_MSG, payload: { flashMsg: { error: true, displayMsg: true, msg: `raw error msg ... ${error}` } } })
+    // TODO - update with alerts.js redux file
+    dispatch(updateMsg('Failed Signin', {error:true}, 5))
+    // dispatch({ type: FLASH_MSG, payload: { flashMsg: { error: true, displayMsg: true, msg: `raw error msg ... ${error}` } } })
   })
 }
 
 export const localSignIn = (username, password) => (dispatch, getState) => {
-  return axios.post('/auth/login', { username, password }).then((response) => {
+  axios.post('/auth/login', { username, password }).then((response) => {
     // dispatch successful login
     console.log('SUCEESSFUL login')
-    return dispatch({ type: LOCAL_SIGN_IN, payload: { user: response.data.user } })
+    dispatch({ type: LOCAL_SIGN_IN, payload: { user: response.data.user } })
   }).then(() => {
     console.log('about to dispatch flash message')
     // throw new Error('something went wrong hahahhaha')
     // debugger
-    dispatch({ type: FLASH_MSG, payload: { flashMsg: { error: false, displayMsg: true, msg: 'You have successfully signed in :)' } } })
+    // TODO - update with alerts.js redux file
+    // dispatch({ type: FLASH_MSG, payload: { flashMsg: { error: false, displayMsg: true, msg: 'You have successfully signed in :)' } } })
   })
   .catch((error) => {
     // dispatch failed login 
     console.log('ERROR IN THE REQUESR???')
     console.log(error)
-    dispatch({ type: FLASH_MSG, payload: { flashMsg: { error: true, displayMsg: true, msg: `raw error msg ... ${error}` } } })
+    // TODO - update with alerts.js redux file
+    // dispatch({ type: FLASH_MSG, payload: { flashMsg: { error: true, displayMsg: true, msg: `raw error msg ... ${error}` } } })
   })
 }
 
@@ -80,6 +87,7 @@ export const localSignUp = (username, password, optParams = {}) => (dispatch, ge
     console.log('no error ...')
   }).catch((error) => {
     console.log('error')
-    dispatch({ type: FLASH_MSG, payload: { flashMsg: {error: true, displayMsg: true, msg:  `raw error msg ... ${error}`}}})
+    // TODO - update with alerts.js redux file
+    // dispatch({ type: FLASH_MSG, payload: { flashMsg: {error: true, displayMsg: true, msg:  `raw error msg ... ${error}`}}})
   })
 }
