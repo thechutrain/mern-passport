718452b1714e54e34cd8cb640122d3eab84756c5
thechutrain <chu.chuyalan@gmail.com>
2017-08-29 15:12:40 -0400

    updated header to conditionally render links based on whether you're logged in or not

--- src/React/containers/Header/Header.jsx
+++ src/React/containers/Header/Header.jsx
@@ -1,31 +1,35 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 // import  store from '../../../Redux'
 import { signOut } from '../../../Redux/authentication'
 import './Header.css'
 
-class Header extends Component {
-	// constructor(){
-	// 	super()
-	// 	this.Logout = this.Logout.bind(this)
-	// }
-	// Logout(){
-	// 	store.dispatch(signOut())
-	// }
-	render() {
-		return (
-			<div className="Header">
-				<Link to="/register">register</Link>
-				<Link to="/signin">signin</Link>
-				<Link to="/" onClick={this.props.logout}>Logout</Link>
-			</div>
-		)
-	}
+// class Header extends Component {
+// 	render() {
+// 		return (
+// 			<div className="Header">
+// 				<Link to="/">Home</Link>
+// 				<Link to="/register">register</Link>
+// 				<Link to="/signin">signin</Link>
+// 				<Link to="/" onClick={this.props.logout}>Logout</Link>
+// 			</div>
+// 		)
+// 	}
 
+// }
+const Header = props => {
+	return (
+		<div className="Header">
+			<Link to="/">Home</Link>
+			<Link to="/register" className={props.loggedIn ? 'hidden' : null}>register</Link>
+ 			<Link to="/signin" className={props.loggedIn ? 'hidden' : null}>signin</Link>
+			<Link to="/" onClick={props.logout} className={props.loggedIn ? null : 'hidden'}>Logout</Link>
+		</div>
+	)
 }
 
-const mapStateToProps = function(store){
+const mapStateToProps = function (store) {
 	return {
 		loggedIn: store.authenticate.loggedIn,
 		// TODO - user name here ...
@@ -33,7 +37,7 @@ const mapStateToProps = function(store){
 }
 const mapDispatchToProps = (dispatch) => {
 	return {
-		logout: () => { dispatch(signOut())}
+		logout: () => { dispatch(signOut()) }
 	}
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Header)
